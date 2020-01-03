b24a65ad7bf92e51291975725616a9e3d0e0984c
msnodeve <msnodeve@gmail.com>
2020-01-03 22:23:24 +0900

    init spring setting

---
+++ src/main/java/com/example/SpringBackendCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendCrudApplication.class, args);
+	}
+
+}
